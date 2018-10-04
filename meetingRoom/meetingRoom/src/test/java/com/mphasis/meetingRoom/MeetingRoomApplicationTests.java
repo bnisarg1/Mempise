48c5cba7e7007bb5035bb901cf45012520d7831d
bnisarg1 <38997313+bnisarg1@users.noreply.github.com>
2018-10-04 21:27:15 +0530

    Add files via upload

---
+++ meetingRoom/meetingRoom/src/test/java/com/mphasis/meetingRoom/MeetingRoomApplicationTests.java
@@ -0,0 +1,11 @@
+package com.mphasis.meetingRoom;
+
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+public class MeetingRoomApplicationTests {
+
+//	@Test
+//	public void contextLoads() {
+//	}
+
+}
